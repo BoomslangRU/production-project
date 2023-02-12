46715ea13adde42df70e3f328e5b554245a9ef6b
BoomslangRU <eilinals@mail.ru>
2023-02-12 19:43:10 +0300

    Gang size analysis. BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,10 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 import type { BuildOptions } from './types/config'
+
 
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
@@ -19,6 +21,9 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
       new webpack.DefinePlugin({
          __IS_DEV__: JSON.stringify(isDev)
       }),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+         openAnalyzer: false
+      })
    ]
 }
