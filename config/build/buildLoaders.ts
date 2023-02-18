930efbc3d02a0f20d0523013e058cd813320f85b
BoomslangRU <eilinals@mail.ru>
2023-02-18 12:28:16 +0300

    fix import for i18nForTests

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -14,17 +14,8 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
       use: {
          loader: 'babel-loader',
          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-               [
-                  'i18next-extract',
-                  {
-                     locales: ['ru', 'en'],
-                     keyAsDefaultValue: true,
-                  },
-               ],
-            ],
-         },
+            presets: ['@babel/preset-env']
+         }
       }
    }
 
