5ce0003f367c7ebef56264bc6e04ccf051a656d1
BoomslangRU <eilinals@mail.ru>
2023-02-18 11:57:49 +0300

    fix config i18n

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from 'app/App'
-import 'shared/config/i18n'
+import 'shared/config/i18n/i18n'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
