c4955c993762fe24013dc711c4dae7134aca4663
BoomslangRU <eilinals@mail.ru>
2023-02-19 17:24:28 +0300

    add visual tool screenshots

---
+++ scripts/generate-visual-json-report.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { promisify } = require('util')
+const { readdir, writeFile } = require('fs')
+const { join: joinPath, relative } = require('path')
+
+const asyncReaddir = promisify(readdir)
+const writeFileAsync = promisify(writeFile)
+
+// eslint-disable-next-line no-undef
+const lokiDir = joinPath(__dirname, '..', '.loki')
+const actualDir = joinPath(lokiDir, 'current')
+const expectedDir = joinPath(lokiDir, 'reference')
+const diffDir = joinPath(lokiDir, 'difference');
+
+
+(async function main() {
+   const diffs = await asyncReaddir(diffDir)
+
+   await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
+      newItems: [],
+      deletedItems: [],
+      passedItems: [],
+      failedItems: diffs,
+      expectedItems: diffs,
+      actualItems: diffs,
+      diffItems: diffs,
+      actualDir: relative(lokiDir, actualDir),
+      expectedDir: relative(lokiDir, expectedDir),
+      diffDir: relative(lokiDir, diffDir)
+   }))
+})()
